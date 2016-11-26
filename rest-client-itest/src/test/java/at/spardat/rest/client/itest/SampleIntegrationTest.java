44f48bbc7efc1390ade0298716ac0fd95b3e9c82
Christoph Pickl <christoph.pickl@gmail.com>
2016-11-26 14:40:55 +0100

    #6 basic itest module setup

---
+++ rest-client-itest/src/test/java/at/spardat/rest/client/itest/SampleIntegrationTest.java
@@ -0,0 +1,12 @@
+package at.spardat.rest.client.itest;
+
+import org.testng.annotations.Test;
+
+@Test
+public class SampleIntegrationTest {
+
+    public void f() {
+        System.out.println("yes");
+    }
+
+}
